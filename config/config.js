a7708598bd946c489e3bb53d4e9942183b8cc97d
Md Imran <154124258+imoamo@users.noreply.github.com>
2024-09-13 13:41:15 +0530

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables
 
 const dbConnect = () => {
-    mongoose.connect('mongodb+srv://imoamo:imo786yo@cluster0.rl2miul.mongodb.net/AlphaTribe?retryWrites=true&w=majority&appName=Cluster0')
-}
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
+    const uri = `mongodb+srv://${username}:${password}@cluster0.rl2miul.mongodb.net/AlphaTribe?retryWrites=true&w=majority&appName=Cluster0`;
+
+    mongoose.connect(uri);
+};
 
 module.exports = dbConnect;
